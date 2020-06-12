29deaeaaeb596f93b730b2126de00178b5b5b47b
gilang611456 <65783878+gilang611456@users.noreply.github.com>
2020-06-13 01:53:40 +0700

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,8 +1,26 @@
-const Discord = require ('discord.js') ;
-const Client = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const timezone = require('moment-timezone');
 
-Client.on('ready' , () => {
-console.log('Nyala');
+function botStatus() {
+  
+  let waktu2 = client.channels.find('id', '706519225119932507')
+  waktu2.setName(`${timezone().tz("Asia/Jakarta").format("🕰 HH:mm [WIB]") + " "}`)
+  
+  let waktu3 = client.channels.find('id', '706519263514722344')
+  waktu3.setName(`${timezone().tz("Asia/Makassar").format("🕰 HH:mm [WITA]") + " "}`)
+  
+  let waktu4 = client.channels.find('id', '706519293365321728')
+  waktu4.setName(`${timezone().tz("Asia/Jayapura").format("🕰 HH:mm [WIT]") + " "}`)
+  
+}; setInterval(botStatus, 10000)
+
+
+
+client.on('ready' , () => {
+  console.log(`${client.user.username} Siap!`);
+  client.user.setActivity('Yotteno')
 });
 
-Client.login(process.env.BOT)
+
+client.login(process.env.TOKEN)
